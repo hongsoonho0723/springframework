6074c2826a573e91dc26f5e7e0a2f754b8076b86
hongsoonho <hongsoonho0723@gmail.com>
2024-05-16 00:14:20 +0900

    0515ver1

--- step09_MyFrameworkLIB/src/main/java/web/mvc/controller/InsertController.java
+++ step09_MyFrameworkLIB/src/main/java/web/mvc/controller/InsertController.java
@@ -1,6 +1,6 @@
 package web.mvc.controller;
 
-import java.io.IOException; 
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
--- step10_SpringMVC/src/main/webapp/WEB-INF/views/param/test.jsp
+++ step10_SpringMVC/src/main/webapp/WEB-INF/views/param/test.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1> test1.jsp입니다...</h1>
+<h1> test.jsp입니다...</h1>
 </body>
 </html>
